aa94fcc00da7c4d4afd366585009a999c014fbae
Michael Levitin <stgriard@yandex.ru>
2024-02-01 11:03:08 +0300

    extended full name check

--- internal/delivery/delivery.go
+++ internal/delivery/delivery.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Michael-Levitin/YellowPages/internal/dto"
 	"github.com/Michael-Levitin/YellowPages/internal/logic"
 	"net/http"
+	"unicode"
 )
 
 type PagesServer struct {
@@ -18,18 +19,13 @@ func NewPagesServer(logic logic.PagesLogicI) *PagesServer {
 }
 
 func (p PagesServer) SetInfo(w http.ResponseWriter, r *http.Request) {
-	queryParams := r.URL.Query() // Получаем все query параметры из URL запроса
-
-	var info dto.Info
-	info.Name = queryParams.Get("name")
-	info.Surname = queryParams.Get("surname")
-	info.Patronymic = queryParams.Get("patronymic")
-	if info.Name == "" || info.Surname == "" {
-		fmt.Fprintln(w, "both name and surname are required")
+	info, err := checkFIO(r)
+	if err != nil {
+		fmt.Fprintln(w, err)
 		return
 	}
 
-	info, err := p.logic.SetInfo(context.TODO(), info)
+	info, err = p.logic.SetInfo(context.TODO(), info)
 	if err != nil {
 		fmt.Fprintln(w, err)
 		return
@@ -37,4 +33,45 @@ func (p PagesServer) SetInfo(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(info)
+}
+
+func checkFIO(r *http.Request) (dto.Info, error) {
+	var info dto.Info
+	queryParams := r.URL.Query() // Получаем все query параметры из URL запроса
+	info.Name = queryParams.Get("name")
+	info.Surname = queryParams.Get("surname")
+	info.Patronymic = queryParams.Get("patronymic")
+	if info.Name == "" || info.Surname == "" {
+		return dto.Info{}, fmt.Errorf("both name and surname are required")
+	}
+
+	if !isLetter(info.Name) || !isLetter(info.Surname) || !isLetter(info.Patronymic) {
+		return dto.Info{}, fmt.Errorf("full name must contain letters only")
+	}
+
+	if !isCapital(info.Name) || !isCapital(info.Surname) || !isCapital(info.Patronymic) {
+		return dto.Info{}, fmt.Errorf("full name must be capitalizedddd")
+	}
+	return info, nil
+}
+
+func isLetter(s string) bool {
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			return true
+		}
+	}
+	return false
+}
+
+func isCapital(s string) bool {
+	var fl rune
+	for _, r := range s {
+		fl = r
+		break
+	}
+	if unicode.IsUpper(fl) {
+		return true
+	}
+	return false
 }
